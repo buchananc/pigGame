9a6c05784dd77162594543430f95ea8ae40a1be3
buchananc <candacebuchanan7@gmail.com>
2018-10-07 18:17:34 -0400

    new game is working

--- app.js
+++ app.js
@@ -12,17 +12,9 @@ GAME RULES:
 // VARIABLES
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+// clear out all scores
+init();
 
-// DOM MANIPULATION
-
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 // EVENT HANDLERS
 document.querySelector('.btn-roll').addEventListener('click', function () {
@@ -58,6 +50,8 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     if (scores[activePlayer] >= 100) {
         document.querySelector('#name-' + activePlayer).textContent = "Winner!";
         document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 
     } else {
         // Next player
@@ -82,4 +76,33 @@ function nextPlayer() {
 
     // hide die until player rolls
     document.querySelector('.dice').style.display = 'none';
+}
+
+// when new game is clicked, call init function to restart game
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+function init() {
+    // sets all scores to 0
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    // DOM MANIPULATION
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    // remove winner class
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
 }
